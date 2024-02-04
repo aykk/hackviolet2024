a3006ad564576856046fc079bcaab8c98c8f48a9
Leo Kim <leokim7521@gmail.com>
2024-02-04 02:32:38 -0500

    all the prompts

--- src/pages/Service2.tsx
+++ src/pages/Service2.tsx
@@ -3,43 +3,137 @@ import Navbar from '../components/Navbar';
 import OpenAIResponse from '../Response';
 import { useLocation } from 'react-router-dom'; 
 
-const TypingText = ({ text }: { text: string }) => {
-  const [displayedText, setDisplayedText] = useState('');
-
-  useEffect(() => {
-    let index = 0;
-    const typingInterval = setInterval(() => {
-      setDisplayedText((prev) => prev + text[index]);
-      index += 1;
-
-      if (index === text.length) {
-        clearInterval(typingInterval);
-      }
-    }, 15); // Adjust the interval for the typing speed
-
-    return () => clearInterval(typingInterval);
-  }, [text]);
-
-  return <span>{displayedText}</span>;
-};
 
 const Service2 = () => {
-  const [openAIResponse, setOpenAIResponse] = useState<string | undefined>(undefined);
+  const [openAIResponseWR, setOpenAIResponse] = useState<string | undefined>(undefined);
+  const [openAIResponseIM, setOpenAIResponse2] = useState<string | undefined>(undefined);
+  const [openAIResponseEM, setOpenAIResponse3] = useState<string | undefined>(undefined);
+  const [openAIResponseGN, setOpenAIResponse4] = useState<string | undefined>(undefined);
+
+  const [openAIResponseGR, setOpenAIResponse5] = useState<string | undefined>(undefined);
+  const [openAIResponseFM, setOpenAIResponse6] = useState<string | undefined>(undefined);
+  const [openAIResponseWK, setOpenAIResponse7] = useState<string | undefined>(undefined);
+  const [openAIResponseSC, setOpenAIResponse8] = useState<string | undefined>(undefined);
   const location = useLocation();
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const openAI = new OpenAIResponse();
-        const prompt = `You are an assistant going to explain the key differences between ${location.state.country} and the United States specifically in Women's rights in the language of ${location.state.language}`;
+        const prompt = `You are an assistant going to explain the key differences between ${location.state.country}
+         and the United States specifically in Women's rights in the language of ${location.state.language} please be specific yet concise to 2 sentences max.`;
 
         await openAI.fetchData(prompt);
         const generatedString = openAI.getResponse();
+
         setOpenAIResponse(generatedString);
       } catch (error) {
         console.error('Error fetching data:', error);
         setOpenAIResponse('Failed to fetch data');
       }
+
+      try {
+        const openAI = new OpenAIResponse();
+        const prompt = `You are an assistant going to explain the key differences between ${location.state.country}
+         and the United States specifically in immigrations laws and deportation in the language of ${location.state.language} please be specific yet concise to 2 sentences max`;
+
+        await openAI.fetchData(prompt);
+        const generatedString = openAI.getResponse();
+
+        setOpenAIResponse2(generatedString);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+        setOpenAIResponse('Failed to fetch data');
+      }
+
+      try {
+        const openAI = new OpenAIResponse();
+        const prompt = `You are an assistant going to explain the key differences between ${location.state.country}
+         and the United States specifically in employee and employers laws as well as career opportunities in the language of ${location.state.language} please be specific yet concise to 2 sentences max`;
+
+        await openAI.fetchData(prompt);
+        const generatedString = openAI.getResponse();
+
+        setOpenAIResponse3(generatedString);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+        setOpenAIResponse('Failed to fetch data');
+      }
+
+      try {
+        const openAI = new OpenAIResponse();
+        const prompt = `You are an assistant going to explain the key differences between ${location.state.country}
+         and the United States in terms of the most important legal codes and laws. Please respond in the language of ${location.state.language} please be specific yet concise to 2 sentences max`;
+
+        await openAI.fetchData(prompt);
+        const generatedString = openAI.getResponse();
+
+        setOpenAIResponse4(generatedString);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+        setOpenAIResponse('Failed to fetch data');
+      }
+
+      try {
+        const openAI = new OpenAIResponse();
+        const prompt = `You are an assistant going to explain the key differences between ${location.state.country}
+         and the United States in terms of the topic of gender in a social context in the language of ${location.state.language} please be specific yet concise to 2 sentences max`;
+
+        await openAI.fetchData(prompt);
+        const generatedString = openAI.getResponse();
+
+        setOpenAIResponse5(generatedString);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+        setOpenAIResponse('Failed to fetch data');
+      }
+
+      try {
+        const openAI = new OpenAIResponse();
+        const prompt = `You are an assistant going to explain the key differences between ${location.state.country}
+         and the United States in terms of family in a social context in the language of ${location.state.language} please be specific yet concise to 2 sentences max`;
+
+        await openAI.fetchData(prompt);
+        const generatedString = openAI.getResponse();
+
+        setOpenAIResponse6(generatedString);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+        setOpenAIResponse('Failed to fetch data');
+      }
+
+      try {
+        const openAI = new OpenAIResponse();
+        const prompt = `You are an assistant going to explain the key differences between ${location.state.country}
+         and the United States in terms of work in a social context in the language of ${location.state.language} please be specific yet concise to 2 sentences max`;
+
+        await openAI.fetchData(prompt);
+        const generatedString = openAI.getResponse();
+
+        setOpenAIResponse7(generatedString);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+        setOpenAIResponse('Failed to fetch data');
+      }
+
+      try {
+        const openAI = new OpenAIResponse();
+        const prompt = `You are an assistant going to explain the key differences between ${location.state.country}
+         and the United States in terms of the most jarring social and cultural differences in the language of ${location.state.language} please be specific yet concise to 2 sentences max`;
+
+        await openAI.fetchData(prompt);
+        const generatedString = openAI.getResponse();
+
+        setOpenAIResponse8(generatedString);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+        setOpenAIResponse('Failed to fetch data');
+      }
+
+
+
+
+
     };
 
     fetchData();
@@ -48,14 +142,36 @@ const Service2 = () => {
   return (
     <div>
       <Navbar />
-      <h1>Hello from Service2!</h1>
-      {openAIResponse !== undefined ? (
+      <h1>Legal!</h1>
+
         <h2>
-          <TypingText text={openAIResponse || ''} />
+          {openAIResponseWR}
         </h2>
-      ) : (
-        <p>Loading...</p>
-      )}
+        <h2>
+          {openAIResponseIM}
+        </h2>
+        <h2>
+          {openAIResponseEM}
+        </h2>
+        <h2>
+          {openAIResponseGN}
+        </h2>
+
+
+
+        <h1>Cultural!</h1>
+        <h2>
+          {openAIResponseGR}
+        </h2>
+        <h2>
+          {openAIResponseFM}
+        </h2>
+        <h2>
+          {openAIResponseWK}
+        </h2>
+        <h2>
+          {openAIResponseSC}
+        </h2>
     </div>
   );
 };
