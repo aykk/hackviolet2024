086acb63d5f9ab6cb2ed9f3e6dec452fa7d620ea
ayk <jungmink623@gmail.com>
2024-02-04 01:27:30 -0500

    landing page changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from './pages/Home';
 import Service from "./pages/Service";
 import Service2 from "./pages/Service2";
-import Chat from "./components/Chat"
+import AboutUs from "./pages/AboutUs";
+import Resources from "./pages/Resources";
 
 function App() {
   return (
@@ -11,9 +12,9 @@ function App() {
           <Routes>
             <Route path="/" element={<Home/>}/>
             <Route path="/service" element={<Service/>}/>
-            {/* <Route path="/response" element={<Service2/>}/> */}
-            <Route path="/" element={<Chat />} />
-            <Route path="/pages/Service2" element={<Service2 />} />
+            <Route path="/pages/response" element={<Service2 />} />
+            <Route path="/aboutus" element={<AboutUs/>}/>
+            <Route path="/resources" element={<Resources/>}/>
           </Routes>
         </BrowserRouter>
     </div>
